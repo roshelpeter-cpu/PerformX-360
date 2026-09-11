506425f61c0874df30e2496152c317523ea6a9ff
roshelpeter-cpu <roshelpeter@gmail.com>
2026-09-11 17:17:49 +0530

    fix backend production startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import "dotenv/config";
 import app from "./app.js";
-import { env } from "./config/env.js";
 
-app.listen(env.port, () => {
-  console.log(`Server running on http://localhost:${env.port}`);
-});
+export default app;
+
+if (process.env.NODE_ENV !== "production") {
+  const { env } = require("./config/env");
+  
+  app.listen(env.port, () => {
+    console.log(`Server running on http://localhost:${env.port}`);
+  });
+}
